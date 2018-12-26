ba798835fb25e6560bdd0d6ed3b54fe64826a7e4
edson <edsaraujoc@gmail.com>
2018-12-26 09:36:31 -0200

    Some numbers operations

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
-print('Yankees rule!')
-print('But not in Boston!')
-print('Yankees rule!','But not in Boston')
+i = 10
+j = 20
+
+print('Is the sum of i and j:',i+j)
+print('Is i minus j:',i-j)
+print('Is the product of i and j:',i*j)
+print('Is integer division:',i//j)
+print('Is i divided by j:',i/j)
+print('i mod j',i%j)
+print('Is i rased to the power j:',i**j)
+
